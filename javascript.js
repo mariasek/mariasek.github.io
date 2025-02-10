550b8fc8647fe4512d89b05565005e19894b80e3
autak <jonakrob@gmail.com>
2025-02-10 01:16:52 +0100

    download index test

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-alert("ok!");
+
+document.addEventListener('DOMContentLoaded', function() {
+    //document.getElementById('test').textContent = localStorage.getItem('test-key');
+}, false);
+
+function downloadIndex() {
+    const data = 'Hello, world!';
+    const blob = new Blob([data], { type: 'text/plain' });
+    const fileURL = URL.createObjectURL(blob);
+    const downloadLink = document.createElement('a');
+    downloadLink.href = fileURL;
+    downloadLink.download = 'example.txt';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+}
