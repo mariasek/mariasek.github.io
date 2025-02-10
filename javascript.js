c853475dc58aec44d6dc6e5dfbd8c10c45c21bde
autak <jonakrob@gmail.com>
2025-02-10 01:20:05 +0100

    test

--- javascript.js
+++ javascript.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
 }, false);
 
 function downloadIndex() {
-    const data = 'Hello, world!';
+    //const data = 'Hello, world!';
+    const data = localStorage.getItem('test-key');
     const blob = new Blob([data], { type: 'text/plain' });
     const fileURL = URL.createObjectURL(blob);
     const downloadLink = document.createElement('a');
